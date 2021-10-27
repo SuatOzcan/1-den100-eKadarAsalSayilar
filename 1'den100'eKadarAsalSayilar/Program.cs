6cea4a78165a3557da660e2e7886ea7c22b55b12
Suat Ozcan <41187131+SuatOzcan@users.noreply.github.com>
2021-10-27 13:17:32 +0300

    Initial commit

---
+++ 1'den100'eKadarAsalSayilar/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1_den100_eKadarAsalSayilar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int count;
+            List<int> asalSayilar = new List<int>();
+
+            Console.WriteLine("Birden yüze kadar asal sayılar:");
+            for (number = 1; number <= 100; number++)
+            {
+                count = 0;
+
+                for (int i = 2 ; i < number-1; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 0 && number != 1)
+                {
+
+                    asalSayilar.Add(number);
+                }
+            }
+            foreach (var asalsayi in asalSayilar)
+            {
+                Console.WriteLine(asalsayi);
+            }
+        }
+    }
+}
